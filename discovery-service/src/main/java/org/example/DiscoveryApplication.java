eaa225464baf1b9c638877d451125ec5d5e051a7
Elizaveta Gornovova <l.gornovova@yandex.ru>
2025-10-15 11:20:18 +0300

    added jwt in order configs

--- discovery-service/src/main/java/org/example/DiscoveryApplication.java
+++ discovery-service/src/main/java/org/example/DiscoveryApplication.java
@@ -3,10 +3,11 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 //import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//@EnableEurekaServer
+@EnableEurekaServer
 public class DiscoveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class, args);
